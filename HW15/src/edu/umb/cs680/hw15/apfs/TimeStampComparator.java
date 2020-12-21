aa9e58672e6ce0b7bf968f9c3e113dc58baac684
drhyde488 <36546909+drhyde488@users.noreply.github.com>
2020-12-21 13:21:17 -0500

    Delete TimeStampComparator.java

--- HW15/src/edu/umb/cs680/hw15/apfs/TimeStampComparator.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw15.apfs;
-
-import java.util.Comparator;
-
-public class TimeStampComparator implements Comparator<ApfsElement> {
-    public int compare(ApfsElement element1, ApfsElement element2) {
-        return element1.getLastModified().compareTo(element2.getLastModified());
-    }
-}
